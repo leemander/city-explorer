b4d6754aad197063d0e8a73cb5f82bf832e64f3e
Lee Mander <lee.mander@gmail.com>
2023-10-30 15:53:49 +0000

    Add ability to close error by clicking bg

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,10 @@
 export default function ({ message, setErrorMessage }) {
+  function handleClick(e) {
+    e.target.className === "error-backdrop" && setErrorMessage("");
+  }
+
   return (
-    <div className="error-backdrop">
+    <div className="error-backdrop" onClick={(e) => handleClick(e)}>
       <dialog>
         <h2>Error</h2>
         <p>{message}</p>
