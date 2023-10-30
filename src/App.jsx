bf3d31fb2cc81a5960fb2bed913b5a2e05b48bc1
Lee Mander <lee.mander@gmail.com>
2023-10-30 13:51:12 +0000

    Create form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,50 @@
 import { useState } from "react";
 import "./App.css";
 
+import axios from "axios";
+
 function App() {
-  return <></>;
+  const [location, setLocation] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  async function getLocation(e) {
+    e.preventDefault();
+    const res = await axios.get(
+      `https://eu1.locationiq.com/v1/search?key=${API_KEY}&q=${searchTerm}&format=json`
+    );
+    console.log(res);
+  }
+
+  return (
+    <>
+      <main>
+        <form onSubmit={(e) => getLocation(e)}>
+          <label htmlFor="location">
+            City:
+            <input
+              type="text"
+              id="location"
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+            />
+          </label>
+          <button>Explore!</button>
+        </form>
+        {location && (
+          <article>
+            <h2>{location.display_name}</h2>
+            <ul>
+              <li>Latitude: {location.lat}</li>
+              <li>Longitude: {location.lon}</li>
+            </ul>
+          </article>
+        )}
+      </main>
+    </>
+  );
 }
 
 export default App;
