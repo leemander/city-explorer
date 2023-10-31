2a3250f41ecf556c4adf0c8d9496e825887c4792
Lee Mander <lee.mander@gmail.com>
2023-10-31 15:26:11 +0000

    Create weather endpoint

--- server/server.js
+++ server/server.js
@@ -6,4 +6,19 @@ const app = express();
 
 app.use(cors());
 
+const weatherData = require("./data/weather.json");
+
+function filterWeather(lat, lon, searchQuery) {
+  return weatherData.find((item) => {
+    return (
+      item.lat === lat && item.lon === lon && item.city_name === searchQuery
+    );
+  });
+}
+
+app.get("/weather", (req, res) => {
+  const { lat, lon, searchQuery } = req.query;
+  res.json(filterWeather(lat, lon, searchQuery));
+});
+
 app.listen(PORT, () => console.log(`App is running on port ${PORT}.`));
